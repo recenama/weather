91603e215126ec54d42a8096a3dbe25b1dc56bb4
recenama <cajilemapaola@gmail.com>
2021-02-16 23:25:04 -0500

    feat: fetch data from weather API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-function App() {
+const API_KEY="API_KEY";
+
+const App = ()=> {
+  const [data,setData]= useState({});
+  useEffect(()=>{
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Riobamba&appid=${API_KEY}`).then(res=>{
+      setData(res);
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {JSON.stringify(data.data)}
+    </>
   );
 }
 
